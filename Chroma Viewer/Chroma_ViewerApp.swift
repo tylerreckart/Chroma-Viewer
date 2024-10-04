a2352637042904fb8e723274464c8ab0f1cf2006
Tyler Reckart <tyler.reckart@gmail.com>
2024-10-03 21:37:15 -0400

    Initial Commit

---
+++ Chroma Viewer/Chroma_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chroma_ViewerApp.swift
+//  Chroma Viewer
+//
+//  Created by Tyler Reckart on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Chroma_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
